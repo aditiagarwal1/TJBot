b24f523aaec7354e7af6637a820f6c4e39c4dbb6
aditiagarwal1 <aditiagarwal2020@u.northwestern.edu>
2017-07-21 01:10:07 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+// Please replace the username and password with your bluemix credentials
+
+// Credentials for Vision Api
+exports.VisionKey = 'df447d2fc3abbbf7a675cc5977b8482bfe9130ef' ;
+exports.VisionVersion = '2016-05-20' ;
+
+//Credentials for Watson Text to Speech service
+exports.TTSUsername = '26854923-f747-48ff-878a-a14722403d2a' ;
+exports.TTSPassword = 't4zedsF6VBV6' ;
+
+exports.version = 'v1' ;
